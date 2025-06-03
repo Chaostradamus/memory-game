a4375101f381ae7d29aa2f37afa43d109a4b2b4b
whimsy <lofu6@yahoo.com>
2025-06-03 01:59:23 -0400

    memory game update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 function App() {
   return (
-
-    <div>Memory Game from scratch for Odin Project</div>
+    <>
+      <div>Memory Game from scratch for Odin Project</div>
+      <div>can you spot the differences?</div>
+    </>
   );
 }
 
