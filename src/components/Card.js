8466438e385eb24c99ae8da2f5ad66846d94ff9a
whimsy <lofu6@yahoo.com>
2025-06-11 23:37:18 -0400

    finished

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ const Card = ({name, image, onClick}) => {
     return (
     <div className='card' onClick={onClick}>
         <img src={image} alt={name}/>
-        <p>{name}</p>
+        <p>Digimon: {name}</p>
     </div>
     )
 }
